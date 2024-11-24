d8f713c5a8feaa17d4b239d66e72eb3d9a5b5034
Marcel Zeininger <34206506+Fumlop@users.noreply.github.com>
2024-11-24 18:08:02 +0100

    Update load.py

--- load.py
+++ load.py
@@ -4,8 +4,11 @@ import sys
 import json
 import myNotebook as nb
 import re
+from power_info_window import show_power_info
 from os import path
 from config import config
+import logging
+from config import appname
 from datetime import datetime, timedelta
 
 this = sys.modules[__name__]  # For holding module globals
@@ -16,6 +19,27 @@ this.lastSysPP = {"ABC":{"merits":0, "influence": 0}}
 this.currentSystem = "" 
 this.lastSystem = ""
 this.version = 'v0.1.0'
+# This could also be returned from plugin_start3()
+plugin_name = os.path.basename(os.path.dirname(__file__))
+
+# A Logger is used per 'found' plugin to make it easy to include the plugin's
+# folder name in the logging output format.
+# NB: plugin_name here *must* be the plugin's folder name as per the preceding
+#     code, else the logger won't be properly set up.
+logger = logging.getLogger(f'{appname}.{plugin_name}')
+
+# If the Logger has handlers then it was already set up by the core code, else
+# it needs setting up here.
+if not logger.hasHandlers():
+    level = logging.DEBUG  # So logger.info(...) is equivalent to print()
+
+    logger.setLevel(level)
+    logger_channel = logging.StreamHandler()
+    logger_formatter = logging.Formatter(f'%(asctime)s - %(name)s - %(levelname)s - %(module)s:%(lineno)d:%(funcName)s: %(message)s')
+    logger_formatter.default_time_format = '%Y-%m-%d %H:%M:%S'
+    logger_formatter.default_msec_format = '%s.%03d'
+    logger_channel.setFormatter(logger_formatter)
+    logger.addHandler(logger_channel)
 
 def plugin_start3(plugin_dir):
     directory_name = path.basename(path.dirname(__file__))
@@ -30,6 +54,7 @@ def plugin_start3(plugin_dir):
         "LastUpdate": "",
         "AccumulatedMerits": 0,
         "AccumulatedSince": "",
+        "Systems":{}
     }
 
     # JSON prüfen oder initialisieren
@@ -49,25 +74,70 @@ def plugin_start3(plugin_dir):
 
 def plugin_app(parent):
     # Adds to the main page UI
+    this.currentSystem = "TEST"
+    this.lastSystem = "LAST"
+    this.currentSysPP = { "TEST" :{"sessionMerits":0, "influence": 0}}
+    this.lastSysPP = { "LAST" :{"sessionMerits":0, "influence": 0}}
+
     this.frame = tk.Frame(parent)
-    this.title = tk.Label(this.frame, text="Powerplay and merits".strip(), anchor="w", justify="left")
-    this.power = tk.Label(this.frame, text=f"Pledged power : {this.powerInfo['PowerName']}".strip(), anchor="w", justify="left")
-    this.powerrank = tk.Label(this.frame, text=f"Rank : {this.powerInfo['Rank']}".strip(), anchor="w", justify="left")
+    this.power = tk.Label(this.frame, text=f"Pledged power : {this.powerInfo['PowerName']} - Rank : {this.powerInfo['Rank']}".strip(), anchor="w", justify="left")
     this.currMerits = tk.Label(this.frame, text=f"Current merits : {this.powerInfo['Merits']}".strip(), anchor="w", justify="left")
     this.meritsLastSession = tk.Label(this.frame, text=f"Last session merits : {this.powerInfo['AccumulatedMerits']}".strip(), anchor="w", justify="left")
-    this.currentSystemLabel = tk.Label(this.frame, text=f"Current system : 0".strip(), anchor="w", justify="left")
-    this.lastSystemLabel = tk.Label(this.frame, text=f"Last system : 0".strip(), anchor="w", justify="left")
+    this.currentSystemLabel = tk.Label(this.frame, text=f"Current system '{this.currentSystem}'".strip(), anchor="w", justify="left")
+    this.currentSystemEntry = tk.Entry(this.frame, width=10)
+    this.currentSystemButton = tk.Button(this.frame, text="OK", command=lambda: [update_system_merits(this.currentSystem, this.currentSystemEntry.get()), update_display()])
+
+    this.currentSystemLabel.grid(row=4, column=0, sticky='we')
+    this.currentSystemEntry.grid(row=4, column=1, padx=5, sticky='we')
+    this.currentSystemButton.grid(row=4, column=2, padx=5, sticky='we')
+    this.lastSystemLabel = tk.Label(this.frame, text=f"Last system '{this.lastSystem}'".strip(), anchor="w", justify="left")
 
     # Positionierung der Labels
-    this.title.grid(row=0, column=0, sticky='we')
-    this.power.grid(row=1, column=0, sticky='we')
-    this.powerrank.grid(row=2, column=0, sticky='we')
-    this.currMerits.grid(row=3, column=0, sticky='we')
-    this.meritsLastSession.grid(row=4, column=0, sticky='we')
-    this.currentSystemLabel.grid(row=5, column=0, sticky='we')
-    this.lastSystemLabel.grid(row=6, column=0, sticky='we')
-    
+    this.power.grid(row=0, column=0, sticky='we')
+    this.currMerits.grid(row=1, column=0, sticky='we')
+    this.meritsLastSession.grid(row=2, column=0, sticky='we')
+    this.currentSystemLabel.grid(row=3, column=0, sticky='we')
+    this.lastSystemLabel.grid(row=4, column=0, sticky='we')
+    # Button zum Anzeigen der Power Info
+    this.showButton = tk.Button(
+        this.frame,  # Button wird zu `this.frame` hinzugefügt
+        text="Show Merits",
+        command=lambda: show_power_info(parent, this.powerInfo)
+    )
+    this.showButton.grid(row=5, column=0, sticky='we', pady=10)
+    parent.bind("<Configure>", resize_button)
     return this.frame
+
+def resize_button(event):
+    parent_width = event.width
+    parent_height = event.height
+    logger.debug('resize_button')    
+    # Berechnung der Button-Größe basierend auf der Parent-Größe
+    button_width = max(5, int(parent_width * 0.1 / 10))  # Mindestens 5 Zeichen breit
+    button_height = max(1, int(parent_height * 0.05 / 20))  # Mindestens 1 Zeile hoch
+
+    this.showButton.config(width=button_width, height=button_height)
+
+
+def update_system_merits(current_system, merits_value):
+    logger.debug('update_merits_value', merits_value)    
+    logger.debug('update_current_system', current_system)    
+    try:
+        merits = int(merits_value)
+
+        # Initialisiere das Systems-Objekt in powerInfo, falls es nicht existiert
+        if "Systems" not in this.powerInfo:
+            this.powerInfo["Systems"] = {}
+
+        # Aktualisiere Merits im aktuellen System
+        if current_system in this.powerInfo["Systems"]:
+            this.powerInfo["Systems"][current_system]["sessionMerits"] += merits
+        else:
+            this.powerInfo["Systems"][current_system] = {"sessionMerits": merits}
+        
+        logger.debug(f"Updated system '{current_system}': {this.powerInfo['Systems'][current_system]}")
+    except ValueError:
+        logger.debug("Invalid merits value. Please enter a number.")
 
 
 def plugin_prefs(parent, cmdr, is_beta):
@@ -79,13 +149,16 @@ def prefs_changed(cmdr, is_beta):
 	update_display()
 
 def update_json_file():
-    """Schreibt die aktuelle powerInfo-Datenstruktur in die JSON-Datei."""
+    logger.debug("update_json_file")
     directory_name = path.basename(path.dirname(__file__))
     plugin_path = path.join(config.plugin_dir, directory_name)
     file_path = path.join(plugin_path, "power.json")
+    power_info_copy = this.powerInfo.copy()
+    if "Systems" in power_info_copy:
+        del power_info_copy["Systems"]
 
     with open(file_path, "w") as json_file:
-        json.dump(this.powerInfo, json_file, indent=4)
+        json.dump(power_info_copy, json_file, indent=4)
 
 def journal_entry(cmdr, is_beta, system, station, entry, state):
     if entry['event'] == 'Powerplay':
@@ -141,13 +214,15 @@ def journal_entry(cmdr, is_beta, system, station, entry, state):
         this.lastSysPP = this.currentSysPP
         this.lastSystem = this.currentSystem
         this.currentSystem = entry.get('StarSystem',"")
-        this.currentSysPP = { this.currentSystem :{"merits":0, "influence": 0}}
+        this.currentSysPP = { this.currentSystem :{"sessionMerits":0}}
+    if entry['event'] == 'Location':
+        this.currentSystem = entry.get('StarSystem',"")
+        this.currentSysPP = { this.currentSystem :{"sessionMerits":0}}
+
 
 
 def update_display():
-    this.title["text"] = "Powerplay and merits".strip()
-    this.title.grid()
-
+    logger.debug("update_display")
     this.power["text"] = f"Pledged power: {this.powerInfo['PowerName'].strip()}".strip()
     this.power.grid()
 
@@ -159,9 +234,9 @@ def update_display():
 
     this.meritsLastSession["text"] = f"Last session merits : {str(this.powerInfo['AccumulatedMerits']).strip()}".strip()
     this.meritsLastSession.grid()
-
-    this.currentSystemLabel["text"] = f"Current system : 0".strip()
+    curr_system_merits = this.powerInfo.get("Systems", {}).get(this.currentSystem, {}).get("sessionMerits", 0)
+    this.currentSystemLabel["text"] = f"Current system '{this.currentSystem}' (Merits: {curr_system_merits})".strip()
     this.currentSystemLabel.grid()
-    
-    this.lastSystemLabel["text"] = f"Last system : 0".strip()
+    last_system_merits = this.powerInfo.get("Systems", {}).get(this.lastSystem, {}).get("sessionMerits", 0)
+    this.lastSystemLabel["text"] = f"Last system '{this.lastSystem}' (Merits: {last_system_merits})".strip()
     this.lastSystemLabel.grid()
