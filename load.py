9f071cb3e20d7bf99bb2e458a4aac746d377e92b
Grumlop <34206506+Fumlop@users.noreply.github.com>
2024-11-27 20:12:08 +0100

    Update load.py

--- load.py
+++ load.py
@@ -39,7 +39,7 @@ else:
     this.currentSysPP = {}
     this.currentSystem = "" 
     this.trackedMerits = 0
-this.version = 'v0.2.6'
+this.version = 'v0.2.7'
 
 # This could also be returned from plugin_start3()
 plugin_name = os.path.basename(os.path.dirname(__file__))
