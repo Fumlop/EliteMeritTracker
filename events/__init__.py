cf87292daa100882e5b8d150ada313554049e0eb
Grumlop <34206506+Fumlop@users.noreply.github.com>
2025-02-22 14:03:12 +0100

    Update __init__.py

--- events/__init__.py
+++ events/__init__.py
@@ -1,7 +1,6 @@
 import os
 import glob
 
-# Alle .py-Dateien im aktuellen Verzeichnis finden (außer __init__.py)
 modules = glob.glob(os.path.join(os.path.dirname(__file__), "*.py"))
 __all__ = [os.path.basename(f)[:-3] for f in modules if os.path.isfile(f) and not f.endswith("__init__.py")]
 
