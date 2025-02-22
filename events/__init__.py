9038679aeb8a34d5d0a8bb609c26c9e4f0c49375
Grumlop <waldpostfach@gmail.com>
2025-02-22 20:37:49 +0100

    Fix Imports

--- events/__init__.py
+++ events/__init__.py
@@ -1,9 +1,6 @@
-import os
-import glob
-
-modules = glob.glob(os.path.join(os.path.dirname(__file__), "*.py"))
-__all__ = [os.path.basename(f)[:-3] for f in modules if os.path.isfile(f) and not f.endswith("__init__.py")]
-
-# Alle Module importieren
-for module in __all__:
-    __import__(f"{__name__}.{module}")
+# events/__init__.py
+from .combatAndScan import *
+from .hack import *
+from .market import *
+from .mission import *
+from .salvage import *
--- events/salvage.py
+++ events/salvage.py
@@ -3,6 +3,7 @@ from imports import *
 this = sys.modules[__name__]  # For holding module globals
 this.debug = False
 
+
 def handleSalvage(entry, factors):
     logger.debug("entry['event'] in ['handleSalvage']")
     merits = factors["Salvage" ][entry['Name']]
