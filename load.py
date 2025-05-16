68e5a9dbbd40a4bc2973acc2c38327417805974c
Grumlop <34206506+Fumlop@users.noreply.github.com>
2025-05-16 16:24:43 +0200

    Update load.py

--- load.py
+++ load.py
@@ -11,7 +11,7 @@ this.dump_test = False
 this.systems = {}
 this.pledgedPower = PledgedPower()
 this.currentSystemFlying = None
-this.version = 'v0.4.67.1.200'
+this.version = 'v0.4.66.1.200'
 this.crow = -1
 this.mainframerow = -1
 this.copyText = tk.StringVar(value=configPlugin.copyText if isinstance(configPlugin.copyText, str) else configPlugin.copyText.get())
@@ -396,7 +396,7 @@ def journal_entry(cmdr, is_beta, system, station, entry, state):
         update_display()
 
 def updateSystemTracker(oldSystem, newSystem):
-    if (oldSystem.StarSystem):
+    if (oldSystem):
         this.systems[oldSystem.StarSystem].Active = False
 
     this.systems[newSystem.StarSystem].Active = True
