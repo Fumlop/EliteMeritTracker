722e1cbf50b8c1aca5526aefedc765743deb4146
Fumlop <waldpostfach@gmail.com>
2025-06-07 23:04:12 +0200

    Bump version to v0.4.71.1.200

--- pluginConfig.py
+++ pluginConfig.py
@@ -15,7 +15,7 @@ class ConfigPlugin:
         self.discordHook= tk.StringVar(value=config.get_str("discordHook") or "")
         self.reportSave: bool = config.get_bool("reportSave") or True
         self.never: bool = config.get_bool("never") or False
-        self.version: str = 'v0.4.70.1.200'
+        self.version: str = 'v0.4.71.1.200'
 
     def getTextCopy(self):
         return "@Leadership earned @MeritsValue merits in @System, @CPControlling, @CPOpposition"
