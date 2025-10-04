3488ace90b214998b920616e587d97ec738e6b3b
Fumlop <waldpostfach@gmail.com>
2025-10-04 10:44:33 +0200

    Version raise

--- pluginConfig.py
+++ pluginConfig.py
@@ -4,7 +4,7 @@ from config import config
 
 class ConfigPlugin:
     def __init__(self):
-        self.version = 'v0.4.200.1.100'
+        self.version = 'v0.4.200.1.101'
         self.loadConfig()
 
     def getTextCopy(self):
