a7d174cb42e73208dfdfd10ac20d22668f23af6b
Fumlop <waldpostfach@gmail.com>
2025-12-16 21:13:42 +0100

    Fixero

--- models/__init__.py
+++ models/__init__.py
@@ -3,4 +3,4 @@ from .system import systems, StarSystem, loadSystems, dumpSystems
 from .power import pledgedPower
 from .backpack import playerBackpack, save_backpack, load_backpack
 from .salvage import Salvage, salvageInventory, save_salvage, load_salvage, VALID_POWERPLAY_SALVAGE_TYPES
-from .ppcargo import ppcargo
+from .ppcargo import Cargo
